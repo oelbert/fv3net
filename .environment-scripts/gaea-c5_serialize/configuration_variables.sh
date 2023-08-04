7d35a53a0f301423b3510f70b9935a8a95b9bbb8
Oliver Elbert <Oliver.Elbert@noaa.gov>
2023-08-04 13:23:36 -0400

    updating config for gaea gnu seriali

--- .environment-scripts/gaea-c5_serialize/configuration_variables.sh
+++ .environment-scripts/gaea-c5_serialize/configuration_variables.sh
@@ -6,26 +6,26 @@ NCEPLIBS_COMPILER=gnu
 
 # ESMF arguments and environment variables
 ESMF_OS=Unicos
-ESMF_COMPILER=gnu
+ESMF_COMPILER=gfortran
 ESMF_SITE=default
 ESMF_CC=cc
 ESMF_COMM=mpiuni
 
 # FMS environment variables
-FMS_CC=cc
-FMS_FC=ftn
+FMS_CC=mpicc
+FMS_FC=mpif90
 FMS_LDFLAGS=
 FMS_LOG_DRIVER_FLAGS=
 FMS_CPPFLAGS='-Duse_LARGEFILE -DMAXFIELDMETHODS_=500 -DGFS_PHYS'
-FMS_FCFLAGS='-FR -i4 -r8'
+FMS_FCFLAGS='-FR -i4 -r8 -fopenmp'
 FMS_MAKE_OPTIONS=
 
 # fv3gfs-fortran arguments
 FV3GFS_PLATFORM=gaea-c5_serialize
 
 # Python requirements environment variables
-MPI4PY_CC=cc
-MPI4PY_MPICC=cc
+MPI4PY_CC=mpicc
+MPI4PY_MPICC=mpicc
 
 # Python wrapper environment variables
 WRAPPER_CC=cc
